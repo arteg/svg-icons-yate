2664131e49618efc39bcca48bb62f56ca8adcd99
b-arteg <b-arteg@yandex-team.ru>
2015-01-30 22:26:48 +0300

    Wrapping svg-sprites with `:::` to seamlessly inline those in yate templates

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,6 +8,7 @@ var open = require('open');
 var gulpGrunt = require('gulp-grunt');
 var gruntTasks = gulpGrunt.tasks();
 // add all the gruntfile tasks to gulp
+var insert = require('gulp-insert');
 gulpGrunt(gulp);
 
 var SRC = {
@@ -17,6 +18,9 @@ var SRC = {
 };
 var DEST = './out/';
 var INDEX_PAGE = DEST + 'index.html';
+var DEST_ICONS = DEST + 'icons/';
+var DEST_SVG_ICONS = DEST_ICONS + '**/*.svg';
+var DEST_SVG_ICONS_YATE_SAFE = DEST_ICONS + 'yate_safe/';
 
 gulp.task('clean', function(cb) {
     del([
@@ -52,3 +56,16 @@ gulp.task('watch', function() {
 gulp.task('open-index-page', function() {
     open(INDEX_PAGE);
 });
+
+/**
+ * Wrapping svg-sprites with `:::` to seamlessly inline those in yate templates.
+ * Needed because svg files can contain inline styles which can be misinterpreted by yate.
+ *
+ * @see https://github.com/pasaran/yate/commit/843cba7b2b3d85362961a95f04674a233f991c48
+ */
+gulp.task('svg-yate-safe', function() {
+    return gulp.src(DEST_SVG_ICONS)
+        .pipe(insert.wrap(':::', ':::'))
+        .pipe(gulp.dest(DEST_SVG_ICONS_YATE_SAFE));
+});
+
